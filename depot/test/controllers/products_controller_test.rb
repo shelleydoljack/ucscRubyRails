aab3788fe3decdb54987bd7e087e357202a6498c
Shelley Doljack <sdoljack@stanford.edu>
2016-08-10 14:39:58 -0700

    Asserts that the product list loads to product page.

--- depot/test/controllers/products_controller_test.rb
+++ depot/test/controllers/products_controller_test.rb
@@ -23,6 +23,7 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select 'body', {class: "products"}
   end
 
   test "should get new" do
