7fc42c021a13628581333580419f1d042b03f1f9
Shelley Doljack <sdoljack@stanford.edu>
2016-08-18 00:11:46 -0700

    Fixes migration for adding product price to line items.

--- depot/db/migrate/20160818061755_add_product_price_to_line_item.rb
+++ depot/db/migrate/20160818061755_add_product_price_to_line_item.rb
@@ -2,7 +2,7 @@ class AddProductPriceToLineItem < ActiveRecord::Migration
   def up
     add_column :line_items, :price, :decimal, :precision => 8, :scale => 2
 
-    LineItem.find(:all).each do |lineitem|
+    LineItem.all.each do |lineitem|
       lineitem.update_attribute :price, lineitem.product.price
     end
   end
