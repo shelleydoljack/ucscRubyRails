ecaa8caf8a990d95acb2133ef9b2c86cb7f5fe06
Shelley Doljack <sdoljack@stanford.edu>
2016-08-17 23:49:08 -0700

    Adds up/down status to database migration to add product price to line item.

--- depot/db/migrate/20160818061755_add_product_price_to_line_item.rb
+++ depot/db/migrate/20160818061755_add_product_price_to_line_item.rb
@@ -1,5 +1,13 @@
 class AddProductPriceToLineItem < ActiveRecord::Migration
-  def change
-    add_column :line_items, :price, :decimal
+  def up
+    add_column :line_items, :price, :decimal, :precision => 8, :scale => 2
+
+    LineItem.find(:all).each do |lineitem|
+      lineitem.update_attribute :price, lineitem.product.price
+    end
+  end
+
+  def down
+    remove_column :line_items, :price
   end
 end
