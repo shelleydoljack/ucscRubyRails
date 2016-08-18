8a7ed6e84f9531c4073588f5cfc5a9be4b2fc94d
Shelley Doljack <sdoljack@stanford.edu>
2016-08-18 15:59:36 -0700

    Adds test for adding two unique items to a cart and fixture data.

--- depot/test/models/cart_test.rb
+++ depot/test/models/cart_test.rb
@@ -12,4 +12,15 @@ class CartTest < ActiveSupport::TestCase
     assert_equal cart.total_price, products(:ruby).price
   end
 
+  test "should create two line items when adding two different products" do
+      cart = Cart.create
+
+      cart.add_product(products(:ruby).id, products(:ruby).price).save!
+      cart.add_product(products(:rails).id, products(:rails).price).save!
+      assert_equal cart.line_items.size, 2
+      assert_equal cart.line_items[0].quantity, 1
+      assert_equal cart.line_items[1].quantity, 1
+      assert_equal cart.total_price, products(:ruby).price + products(:rails).price
+  end
+
 end
