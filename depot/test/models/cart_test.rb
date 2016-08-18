3434b494acc26da781d5c138af6ae9bac534e8af
Shelley Doljack <sdoljack@stanford.edu>
2016-08-18 15:51:22 -0700

    Tests adding an item to a cart.

--- depot/test/models/cart_test.rb
+++ depot/test/models/cart_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test "should create a new line item when adding a new product" do
+    cart = Cart.create
+
+    cart.add_product(products(:ruby).id, products(:ruby).price).save!
+    assert_equal cart.line_items.size, 1
+    assert_equal cart.line_items[0].quantity, 1
+    assert_equal cart.total_price, products(:ruby).price
+  end
+
 end
